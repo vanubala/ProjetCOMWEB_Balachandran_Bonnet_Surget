937300e854633bb24a68bca5664340c4f8fef6bc
Tamiy0 <emile.bonnet123@gmail.com>
2025-05-07 10:47:47 +0200

    ajout bdd

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './NavBar'
 
 function Note() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost/react-api/get_notes.php")
+      .then((res) => res.json())
+      .then((data) => setNotes(data))
+      .catch((err) => console.error("Erreur:", err));
+  }, []);
   return (
     <>
-    <Navbar/>
-    <div>Page Note</div>
+      <Navbar/>
+      <ul>
+          {notes.map((etudiant) => (
+            <li key={etudiant.id}>  {etudiant.NOM_Prénom} - {etudiant.Français}</li>
+          ))}
+      </ul>
     </>
   )
 }
