c2cde11236655f7db2ee58bf99235f1927e0eb83
vanubala <vbalachandr@ensc.fr>
2025-05-11 00:25:50 +0200

    style usertype

--- src/pages/UserType.jsx
+++ src/pages/UserType.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './UserType.css'; // Pour le style (optionnel)
+import './UserType.css';
 
 function UserType() {
   const [role, setRole] = useState('');
