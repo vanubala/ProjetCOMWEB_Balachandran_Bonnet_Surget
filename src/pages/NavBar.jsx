3a7c15840db6207d7ab092716ea3d8b89ed75a29
Tamiy0 <emile.bonnet123@gmail.com>
2025-05-10 16:37:35 +0200

    ajout de la base de donnée dans Note et Navbar

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import "./NavBar.css"
 
 
 function Navbar(){
+  const [etudiant, setEtudiant] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost/react-api/get_notes.php")
+      .then((res) => res.json())
+      .then((data) => {
+        const filtre = data.find((etudiant) => etudiant.NOM_Prénom === "Benoît Rivière");
+        setEtudiant(filtre);
+      })
+      .catch((err) => console.error("Erreur:", err));
+  }, []);
     return (
     <nav
     style={{
@@ -17,6 +29,13 @@ function Navbar(){
       <div style={{ display: 'flex', gap: '2rem' }}>
           <Link to='/Accueil'>Accueil</Link>
           <Link to='/Note'>Note</Link>
+      </div>
+      <div style ={{ color: 'white', fontWeight: 'bold'}}>
+        {etudiant ? (
+            <p>{etudiant.NOM_Prénom}</p>
+          ) : (
+            <p>Chargement ou élève non trouvé.</p>
+          )}
       </div>
       <div style={{ color: 'white', fontWeight: 'bold' }}>
           <Link to='/Identification'><i className="fi fi-br-exit"></i></Link>
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -3,11 +3,17 @@ import Navbar from './NavBar'
 
 function Note() {
   const [notes, setNotes] = useState([]);
+  const [etudiant, setEtudiant] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost/react-api/get_notes.php")
       .then((res) => res.json())
-      .then((data) => setNotes(data))
+      .then((data) => {
+        setNotes(data);
+
+        const filtre = data.find((etudiant) => etudiant.NOM_Prénom === "Benoît Rivière");
+        setEtudiant(filtre);
+      })
       .catch((err) => console.error("Erreur:", err));
   }, []);
   return (
@@ -18,6 +24,19 @@ function Note() {
             <li key={etudiant.id}>  {etudiant.NOM_Prénom} - {etudiant.Français} - {etudiant.Date_Naissance}</li>
           ))}
       </ul>
+      <div>
+        {etudiant ? (
+          <ul>
+            <li>Français : {etudiant.Français}</li>
+            <li>Philosophie : {etudiant.Philosophie}</li>
+            <li>Mathématiques : {etudiant.Mathématiques}</li>
+            <li>Biologie : {etudiant.Biologie}</li>
+            <li>Physique : {etudiant.Physique}</li>
+          </ul>
+        ) : (
+          <p>Chargement ou élève non trouvé.</p>
+        )}
+      </div>
     </>
   )
 }
