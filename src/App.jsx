e9a97482e47d7bc32ffa1b16669fd39a634e6de2
vanubala <vbalachandr@ensc.fr>
2025-05-11 23:39:54 +0200

    suppresion loginprof

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Identification from './pages/Identification'
 import Accueil from './pages/Accueil'
 import Note from './pages/Note'
 import UserType from './pages/UserType'
-import IdProf from './pages/LoginProf'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path='/' element={<UserType/>}/>
         <Route path='/Identification' element={<Identification/>}/>
-        <Route path='/LoginProf' element={<IdProf/>}/>
         <Route path='/Accueil' element={<Accueil/>}/>
         <Route path='/Note' element={<Note/>}/>
       </Routes>
