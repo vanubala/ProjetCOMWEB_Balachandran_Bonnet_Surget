bd8a1ea1398f65fa8f9dcca590c9dc374fdae847
vanubala <vbalachandr@ensc.fr>
2025-05-19 18:10:30 +0200

    .

--- react-api/db.php
+++ react-api/db.php
@@ -10,10 +10,5 @@ $conn = new mysqli($host, $user, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-/* Paramètres de connexion une fois sur zzz.phpmyadmin
-$host = 'localhost';        
-$dbname = 'votre identifiant';
-$username = 'votre identifiant';
-$password = 'votre mdp de la partie base de donnée du zzz'; */
 ?>
 
